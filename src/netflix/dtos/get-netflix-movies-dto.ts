4eeaf3802299f77a014c1de95a268c500a789595
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-24 17:31:35 -0300

    feat: Criando DTO GetNetFlixMovie

---
+++ src/netflix/dtos/get-netflix-movies-dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+
+export class GetNetFlixMoviesDTO {
+
+    @IsNotEmpty({
+        message:'Informe seu email'
+    })
+    @IsEmail(undefined,{
+        message:'Informe um email válido'
+    })
+    readonly email: string
+    
+    @IsNotEmpty({
+        message:'Informe sua senha'
+    })
+    @IsString({
+        message:'Informe uma string'
+    })
+    readonly password: string
+
+}
