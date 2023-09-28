1bb9a8e7650d7c00aabfea01b35271de3efb63bd
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-28 16:17:06 -0300

    feat: Importando MovieModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NetflixModule } from './netflix/netflix.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [NetflixModule],
+  imports: [NetflixModule, MovieModule],
   controllers: [],
   providers: [],
 })
