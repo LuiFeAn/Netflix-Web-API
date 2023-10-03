ac66eb36ca4b3db6335649a920f7df15d0cff9f7
Luis <luisfernandogvv@gmail.com>
2023-10-03 18:05:11 -0300

    refactor: Movendo responsabilidades

--- src/netflix/netflix.service.ts
+++ src/netflix/netflix.service.ts
@@ -1,15 +1,74 @@
 import { Injectable } from '@nestjs/common';;
 import { NetflixAuthDto } from './dtos/netflix-auth-dto';
 import { PuppeteerService } from 'src/puppeteer/puppeteer-service';
+import { UnauthorizedException, BadRequestException } from '@nestjs/common/exceptions';
+import { PageInMemoryRepository } from 'src/repositories/page-in-memory-repository';
 
 @Injectable()
 export class NetflixService {
 
-    constructor( private readonly puppeteerService: PuppeteerService ){}
+    constructor( 
+        private readonly puppeteerService: PuppeteerService, 
+        private readonly pageInMemoryRepository: PageInMemoryRepository 
+    ){}
 
     async authenticate(authCrededntials: NetflixAuthDto){
 
-        await this.puppeteerService.netflixAuth(authCrededntials);
+        const page = await this.puppeteerService.authPage(authCrededntials);
+
+        let credentialErr;
+
+        try{
+
+            await page.waitForSelector('.ui-message-container.ui-message-error',{
+                timeout:3000
+            });
+
+            credentialErr = true;
+
+        }catch(err){
+
+            credentialErr = false;
+
+        }
+
+        if( credentialErr ){
+
+            throw new UnauthorizedException('Email ou senha inválido(s)')
+
+        }
+        
+        await page.waitForSelector('.profile-name');
+
+        const profiles = await page.$$('.profile-name');
+
+        let profileFind = false;
+
+        for await(const currentProfile of profiles){
+
+            const profileText = await currentProfile.getProperty('innerHTML');
+
+            const profileName = await profileText.jsonValue();
+
+            if( profileName === authCrededntials.profile ){
+
+                await currentProfile.click();
+
+                profileFind = true;
+
+            }
+
+        }
+
+        if( !profileFind ){
+
+            throw new BadRequestException(`Perfil não encontrado`);
+
+        }
+
+        await page.waitForSelector('.rowTitle.ltr-0');
+
+        this.pageInMemoryRepository.add('authenticated',page);
 
     }
 
--- src/puppeteer/puppeteer-service.ts
+++ src/puppeteer/puppeteer-service.ts
@@ -1,16 +1,13 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { NetflixAuthDto } from 'src/netflix/dtos/netflix-auth-dto';
-import puppeteer, { ElementHandle } from 'puppeteer';
-import { PageInMemoryRepository } from 'src/repositories/page-in-memory-repository';
+import puppeteer, { Page } from 'puppeteer';
 
 @Injectable()
 export class PuppeteerService {
 
-    constructor(private readonly pageInMemoryRepo: PageInMemoryRepository){}
+    async authPage(netflixCredentials: NetflixAuthDto){
 
-    async netflixAuth(netflixCredentials: NetflixAuthDto){
-
-        const { email, password, profile } = netflixCredentials;
+        const { email, password } = netflixCredentials;
 
         const browser = await puppeteer.launch({
             headless:false
@@ -32,59 +29,12 @@ export class PuppeteerService {
 
         await loginButton.click();
 
-        let credentialErr;
+        return page;
 
-        try{
+    }
 
-            await page.waitForSelector('.ui-message-container.ui-message-error',{
-                timeout:3000
-            });
+    miner(page: Page){
 
-            credentialErr = true;
-
-        }catch(err){
-
-            credentialErr = false;
-
-        }
-
-        if( credentialErr ){
-
-            throw new UnauthorizedException('Email ou senha inválido(s)')
-
-        }
-        
-        await page.waitForSelector('.profile-name');
-
-        const profiles = await page.$$('.profile-name');
-
-        let profileFind = false;
-
-        for await(const currentProfile of profiles){
-
-            const profileText = await currentProfile.getProperty('innerHTML');
-
-            const profileName = await profileText.jsonValue();
-
-            if( profileName === profile ){
-
-                await currentProfile.click();
-
-                profileFind = true;
-
-            }
-
-        }
-
-        if( !profileFind ){
-
-            throw new BadRequestException(`Perfil não encontrado`);
-
-        }
-
-        await page.waitForSelector('.rowTitle.ltr-0');
-
-        this.pageInMemoryRepo.add('authenticated',page);
 
 
     }
