2d4cb89e1d8299e599c07458790a7408885a4b60
LuiFeAn <luisfernandogvv@gmail.com>
2023-10-03 22:58:08 -0300

    feat: Aplicando mineração dos filmes em alta da Netflix

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PageInMemoryRepository } from 'src/repositories/page-in-memory-repository';
 
 @Injectable()
@@ -17,6 +17,26 @@ export class MovieService {
         }
 
         await page.goto('https://www.netflix.com/browse/trending-now');
+
+        const movies = await page.evaluate(function(){
+
+            const movies = [ ...(document.querySelectorAll('.ptrack-content')) ].map(function(movie){
+
+                const link = movie.children[0].getAttribute('href');
+
+                const title = (movie.children[0] as HTMLHeadElement).innerText;
+                
+                const img = `https://www.netflix.com${movie.children[0].children[0].children[0].getAttribute('src')}`
+
+                return { title, img, link }
+
+            });
+
+            return movies
+
+        });
+
+        return movies;
 
     }
 
